338b02672a9034e25d67ab8f7572c7b0a28621c1
Fedor Petrov <fedor.petrovc@gmail.com>
2024-07-11 13:45:27 +0300

    Minor updates

--- T-Badge.Api/Infrastructure/DependencyInjection.cs
+++ T-Badge.Api/Infrastructure/DependencyInjection.cs
@@ -33,7 +33,7 @@ public static class DependencyInjection
         // MARK: Maybe it will raise error.
         var jwtSettings = JwtSettings.Default;
         configuration.Bind(JwtSettings.SectionName, jwtSettings);
-
+        
         services.AddSingleton(Options.Create(jwtSettings));
 
         services
